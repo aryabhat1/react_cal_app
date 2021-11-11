69f32ff0b898a897b4a2d9059779bdc0cdc410a8
Aryabhat <sushant.pru@gmail.com>
2021-11-11 18:22:16 +0530

    my commit

--- calculator2/src/App.js
+++ calculator2/src/App.js
@@ -1,47 +1,68 @@
-import './App.css';
-import Button from './components/Button';
+// import { evaluate } from "mathjs";
+import * as math from "mathjs";
+import { useState } from "react";
+
+import "./App.css";
+import Button from "./components/Button";
+import Input from './components/Input';
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join("");
+
+    setResult(math.evaluate(input));
+  };
+
+  const resetInput = (val) => {
+    setText("");
+    setResult("");
+  };
+  
+  const buttonColor = "grey"
+
   return (
     <div className="App">
       <div className="calc-wrapper">
-        <div className="row">
-        <Button symbol='7'/>
-        <Button symbol='8'/>
-        <Button symbol='9'/>
-        <Button symbol="/"/>
-
-        </div>
-
-        <div className="row">
-        <Button symbol='4' />
-        <Button symbol='5' />
-        <Button symbol='6' />
-        <Button symbol='*' />
-
-        </div>
-
-        <div className="row">
-        <Button symbol='1' />
-        <Button symbol='2' />
-        <Button symbol='3' />
-        <Button symbol='+' />
-
-        </div>
-
-        <div className="row">
-        <Button symbol='0' />
-        <Button symbol='.'/>
-        <Button symbol='=' />
-        <Button symbol='-' />
-
-        </div>
+        <Input text={text} result={result} />
         
-      <h1>Hello World!</h1>
+        <div className="row">
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color={buttonColor} handleClick={addToText} />
+        </div>
+
+        <div className="row">
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color={buttonColor} handleClick={addToText} />
+        </div>
+
+        <div className="row">
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="+" color={buttonColor} handleClick={addToText} />
+        </div>
+
+        <div className="row">
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
+          <Button symbol="=" handleClick={calculateResult} />
+          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+        </div>
+        <Button symbol="clear" color="orange" handleClick={resetInput} />
       </div>
-      
     </div>
   );
-}
+};
 
 export default App;
--- calculator2/src/components/Button.js
+++ calculator2/src/components/Button.js
@@ -1,7 +1,17 @@
 import './Button.css';
 
-const Button = ({ symbol }) => {
-    return <div className='button-wrapper'>{symbol}</div>
+const Button = ({ symbol, color, handleClick }) => {
+    return (
+    
+    <div
+    onClick={() => handleClick(symbol)} 
+    className='button-wrapper' 
+    style={{backgroundColor: color}}>
+        {symbol}
+        
+        </div>
+
+    );
 };
 
 export default Button;
